0032243a4588a26250f8abff68574a6225a7bd5a
jorgaravi <jorgaravi@gmail.com>
2025-03-02 20:45:43 -0500

    prueba 3

--- Aplicaciones/Educacion/models.py
+++ Aplicaciones/Educacion/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Carrera(models.Model):
+    id_car=models.AutoField(primary_key=True)
+    nombre_car=models.CharField(max_length=50)
+    fecha_creacion_car=models.DateField()
+    telefono_car=models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.nombre_car
 # Create your models here.
